099b7e216fd14fa85c42b1a544519386475cc560
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2026-05-18 15:24:08 +0200

    models.Team and initial migration

--- cuhasc/models.py
+++ cuhasc/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
-import constants as c
+import cuhasc.base as base
+import cuhasc.constants as c
 
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     token = models.CharField(max_length=c.TOKEN_LENGTH_TEAM)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = base.random_token(c.TOKEN_LENGTH_TEAM)
+        super().save(*args, **kwargs)
