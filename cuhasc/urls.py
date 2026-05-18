ec16ee182b336e9841a5776832d55d7530e879d7
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2026-05-18 15:25:43 +0200

    views.team_create(), template, url, and link on homepage

--- cuhasc/urls.py
+++ cuhasc/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("create_team", views.team_create, name="team_create"),
 ]
--- cuhasc/views.py
+++ cuhasc/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseNotAllowed
+
+from cuhasc.forms import TeamForm
 
 
 def home(request):
     return render(request, "cuhasc/home.html")
+
+
+def team_create(request):
+    if request.method == 'GET':
+        form = TeamForm()
+        return render(request, "cuhasc/team_create.html", {'form': form})
+    elif request.method == 'POST':
+        form = TeamForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        return render(request, "cuhasc/team_create.html", {'form': form})
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
