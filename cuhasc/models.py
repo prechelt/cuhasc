6bc5482e899130641896024e463039489682dbb6
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2026-05-21 13:54:05 +0200

    models.py: Team: add member_token

--- cuhasc/models.py
+++ cuhasc/models.py
@@ -4,11 +4,20 @@ import cuhasc.base as base
 import cuhasc.constants as c
 
 
+def init_member_token() -> str:
+    return base.random_token(c.TOKEN_LENGTH_MEMBER)
+
+
 class Team(models.Model):
     name = models.CharField(max_length=100)
     token = models.CharField(max_length=c.TOKEN_LENGTH_TEAM)
+    member_token = models.CharField(max_length=c.TOKEN_LENGTH_MEMBER, 
+                                    default=init_member_token)
 
     def save(self, *args, **kwargs):
         if not self.token:
             self.token = base.random_token(c.TOKEN_LENGTH_TEAM)
+            self.member_token = base.random_token(c.TOKEN_LENGTH_MEMBER)
         super().save(*args, **kwargs)
+
+
