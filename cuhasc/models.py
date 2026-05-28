c327c3415a13d59c77531a6842961cedac09682d
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2026-05-28 13:17:00 +0200

    models.py: add Member

--- cuhasc/models.py
+++ cuhasc/models.py
@@ -11,13 +11,24 @@ def init_member_token() -> str:
 class Team(models.Model):
     name = models.CharField(max_length=100)
     token = models.CharField(max_length=c.TOKEN_LENGTH_TEAM)
-    member_token = models.CharField(max_length=c.TOKEN_LENGTH_MEMBER, 
+    member_token = models.CharField(max_length=c.TOKEN_LENGTH_MEMBER,
                                     default=init_member_token)
 
     def save(self, *args, **kwargs):
         if not self.token:
             self.token = base.random_token(c.TOKEN_LENGTH_TEAM)
             self.member_token = base.random_token(c.TOKEN_LENGTH_MEMBER)
+        super().save(*args, **kwargs)
+
+
+class Member(models.Model):
+    name = models.CharField(max_length=100)
+    token = models.CharField(max_length=c.TOKEN_LENGTH_MEMBER)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = base.random_token(c.TOKEN_LENGTH_MEMBER)
         super().save(*args, **kwargs)
 
 
