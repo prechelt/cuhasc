fae0d11e36bfcecd1836b5566cfe541d683e5dfe
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2026-06-01 12:00:01 +0200

    forms.py: MemberForm

--- cuhasc/forms.py
+++ cuhasc/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 
-from cuhasc.models import Team
+from cuhasc.models import Member, Team
 
 
 class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
+        fields = ['name']
+
+
+class MemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
         fields = ['name']
