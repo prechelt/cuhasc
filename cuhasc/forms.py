8ff1397de668f6a1ba4d481eb3cd20be4b247c50
Lutz Prechelt <prechelt@inf.fu-berlin.de>
2026-06-04 13:30:33 +0200

    forms.py: QuestionnaireForm

--- cuhasc/forms.py
+++ cuhasc/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from cuhasc.models import Member, Team
+from cuhasc.models import Member, QResult, Team
 
 
 class TeamForm(forms.ModelForm):
@@ -13,3 +13,26 @@ class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ['name']
+
+
+class QuestionnaireForm(forms.Form):
+    def __init__(self, items, scales, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._items = items
+        for item_data in items:
+            labels = scales[item_data['scale']]
+            choices = [(str(i + 1), label or str(i + 1)) for i, label in enumerate(labels)]
+            self.fields[item_data['item']] = forms.ChoiceField(
+                choices=choices,
+                widget=forms.RadioSelect(attrs={'class': 'form-check-input'}),
+                label=f"{item_data['item']}: {item_data['content']}",
+            )
+
+    def save_results(self, member):
+        for item_data in self._items:
+            value = int(self.cleaned_data[item_data['item']])
+            QResult.objects.update_or_create(
+                member=member,
+                item=item_data['item'],
+                defaults={'scale': item_data['scale'], 'value': value},
+            )
